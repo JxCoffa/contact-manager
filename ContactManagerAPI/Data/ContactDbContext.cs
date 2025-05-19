4d6513c8fc6cd562e884ef5c41edea1f21f7ea05
John Coffa <153008577+JohnxCoffa@users.noreply.github.com>
2025-05-19 11:45:36 +0200

    Added a Model and Data
    - DbContext
    - Contact ViewModel

---
+++ ContactManagerAPI/Data/ContactDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ContactManagerAPI.Data
+{
+	public class ContactDbContext : DbContext
+	{
+	}
+}
--- ContactManagerAPI/Models/ViewModels/Contact.cs
+++ ContactManagerAPI/Models/ViewModels/Contact.cs
@@ -5,10 +5,16 @@ namespace ContactManagerAPI.Models.ViewModels
 	public class Contact
 	{
 		public int Id { get; set; }
+		[Required]
 		public string FirstName { get; set; }
+		[Required]
 		public string LastName { get; set; }
+		[Required]
+		[EmailAddress]
 		public string Email { get; set; }
+		[Required]
 		public string PhoneNumber { get; set; }
+		[Required]
 		[Range(1, 2)]
 		public int CategoryId { get; set; }
 	}
