089476e70e03b4110ea7af0642d622e1c8ce4164
John Coffa <153008577+JohnxCoffa@users.noreply.github.com>
2025-05-19 11:14:25 +0200

    Add project files.

---
+++ ContactManagerAPI/Controllers/ContactAPIController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManagerAPI.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class ContactAPIController : ControllerBase
+	{
+	
+	}
+}
---
+++ ContactManagerAPI/Models/ViewModels/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManagerAPI.Models.ViewModels
+{
+	public class Contact
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public string PhoneNumber { get; set; }
+		[Range(1, 2)]
+		public int CategoryId { get; set; }
+	}
+}
